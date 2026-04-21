4525e38fe5f09e5247a263d8e49588f2941ae21f
dattapatil8 <dattapatil862002@gmail.com>
2026-04-21 14:33:38 +0530

    Creating Adding New Data page

--- CRUD/src/AddNewData.jsx
+++ CRUD/src/AddNewData.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react"
+
 export function AddNewData(){
+  const [id,setid]=useState("");
+  const [name,setName]=useState("");
+  const [email,setEmail]=useState("");
+  const DataSub=()=>{
+console.log(id,name,email)
+  }
     return (
     <>
-     <h1 className='text-2xl text-center pt-20'>Adding data</h1>
+    <form action="">
+     <div className="text-center border-2 w-100 m-10 mx-auto">
+
+      <h1 className="text-3xl font-bold text-center m-10">Adding new data</h1>
+      <input className="w-60 border-2 m-2 " type="text" onChange={(e)=>setid(e.target.value)} placeholder="Enter your ID" /><br/>
+            <input className="w-60 border-2 m-2 " type="text"onChange={(e)=>setName(e.target.value)} placeholder="Enter your Name" /><br></br>
+              <input className="w-60 border-2 m-2" type="text" onChange={(e)=>setEmail(e.target.value)} placeholder="Enter your Email" /><br></br>
+              <button className="bg-amber-600 m-4 w-45 h-10 rounded-2xl hover:bg-amber-800 cursor-pointer" onClick={DataSub}>Add New Data</button>
+
+     </div>
+     </form>
     </>
   )
 }
--- CRUD/src/App.jsx
+++ CRUD/src/App.jsx
@@ -7,9 +7,13 @@ function App() {
 
   return (
     <>
-     <div className='text-2xl text-center'>
-     <Link className="" to='/'>Display data</Link>
-     <Link className="pl-8" to='/add'>Adding data</Link>
+     <div className='text-2xl text-center mt-15 flex'>
+      <div className="bg-amber-600  w-50 h-10 rounded-2xl hover:bg-amber-800 cursor-pointer ml-100">
+     <Link  to='/'>Display data</Link>
+     </div>
+     <div className="bg-amber-600  w-50 h-10 rounded-2xl hover:bg-amber-800 cursor-pointer ml-14">
+     <Link  to='/add'>Adding data</Link>
+     </div>
      </div>
      
       <Routes>
